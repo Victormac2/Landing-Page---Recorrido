f6dfc86dcef1ac9383a67fd20a45dea917eae22b
isaiep <85694277+isaiep@users.noreply.github.com>
2025-11-08 23:25:56 -0600

    Update script.js

--- Recorrido UTT/script.js
+++ Recorrido UTT/script.js
@@ -15,3 +15,35 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 });
+//codigo original del navbar
+document.addEventListener("DOMContentLoaded", () => {
+    const navLinks = document.querySelectorAll("nav a");
+  
+    const updateActiveLink = () => {
+      const currentPath = window.location.pathname.split("/").pop() || "index.php"; // Considera "index.php" por defecto si la ruta estÃ¡ vacÃ­a.
+  
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+  
+        // Solo considerar enlaces internos
+        if (link.href.startsWith(window.location.origin)) {
+          const hrefArray = link.href.split("/");
+          const thisPath = hrefArray[hrefArray.length - 1];
+  
+          if (currentPath === thisPath) {
+            link.classList.add("active");
+          }
+        }
+      });
+    };
+  
+    navLinks.forEach((link) => {
+      link.addEventListener("click", (event) => {
+        navLinks.forEach((navLink) => navLink.classList.remove("active"));
+        link.classList.add("active");
+      });
+    });
+  
+    // Inicializa el enlace activo basado en la URL actual
+    updateActiveLink();
+  });
